d8e32946986f8bdcb8d09b21f7a0d79ac1b6d025
Moi1oM <hcloud0806@gmail.com>
2023-05-27 23:18:18 +0900

    spring theory

---
+++ springmvc/src/main/java/hello/springmvc/requestmapping/MappingController.java
@@ -0,0 +1,28 @@
+package hello.springmvc.requestmapping;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MappingController {
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping(value = "/hello-basic", method = RequestMethod.GET)
+    public String helloBasic() {
+        log.info("helloBasic");
+        return "ok";
+    }
+
+    @GetMapping("/mapping/{userId}")
+    public String mappingPath(@PathVariable("userId")String data) {
+        log.info("mappingPath userId={}",data);
+        return "ok";
+    }
+
+    @GetMapping("/mapping/users/{userId}/orders/{orderId}")
+    public String multiMappingPath(@PathVariable String userId, @PathVariable String orderId) {
+        log.info("mappingPath userId={}, orderId={}", userId, orderId);
+        return "ok";
+    }
+}

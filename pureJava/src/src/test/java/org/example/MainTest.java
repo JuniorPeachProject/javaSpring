a0f391a2bce6b488c9828d2d8665ef331953106d
Moi1oM <hcloud0806@gmail.com>
2023-07-26 14:27:04 +0900

    start java8~

---
+++ pureJava/src/src/test/java/org/example/MainTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    @DisplayName("main")
+    void main() {
+
+    }
+}

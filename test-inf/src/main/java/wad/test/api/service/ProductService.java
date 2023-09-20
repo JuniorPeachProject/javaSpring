e3511cbd399c316f2c0c0c4ce575f42e2fbb1e40
Moi1oM <hcloud0806@gmail.com>
2023-09-21 01:04:21 +0900

    feat: Product basic api

---
+++ test-inf/src/main/java/wad/test/api/service/ProductService.java
@@ -0,0 +1,24 @@
+package wad.test.api.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import wad.test.api.service.product.response.ProductResponse;
+import wad.test.domain.product.Product;
+import wad.test.domain.product.ProductRepository;
+import wad.test.domain.product.ProductSellingStatus;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public List<ProductResponse> getSellingProducts() {
+        List<Product> products = productRepository.findAllBySellingStatusIn(ProductSellingStatus.forDisplay());
+        return products.stream()
+                .map(ProductResponse::of)
+                .collect(Collectors.toList());
+    }
+}

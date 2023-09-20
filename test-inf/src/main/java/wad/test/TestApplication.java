77960413016d7a0a11457e179898d5e7b8ea7af5
Moi1oM <hcloud0806@gmail.com>
2023-09-20 18:09:51 +0900

    start test-inf

---
+++ test-inf/src/main/java/wad/test/TestApplication.java
@@ -0,0 +1,13 @@
+package wad.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+
+}
